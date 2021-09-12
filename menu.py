afb7a83a6ca0a1d4637f2e8850b47113558188fe
José Alves <josealves3416@gmail.com>
2021-09-12 20:57:21 -0300

    Verificação de caracteres inapropiados

--- menu.py
+++ menu.py
@@ -34,6 +34,7 @@ class Program:
     
     def loop(self):
         window1, window2 = self.start_window(), None
+        bad_characters = ['@','#','$','!','-','%','*','(',')','&','<','>',';',':','/','^','~','=','+']
         while True:
             #reading values and events
             window, event, values = sg.read_all_windows()
@@ -43,8 +44,18 @@ class Program:
                 window2 = self.rg_window()
                 window1.hide()
             if window == window2 and event == 'Registrar':
-                if len(values['_username_']) < 8 or len(values['password']) < 8:
-                    sg.PopupNoTitlebar('(Nome de usário/Senha) não pode ter menos de 8 caracteres')
+                #temcchar é uma variável do tipo True/False que retorne True se possuir algum caractere indesejado no nome
+                temcchar= False
+                for c in bad_characters:
+                    if c in values['_username_']:
+                        temcchar = True
+
+                if len(values['_username_']) < 8 or len(values['password']) < 8 or temcchar > 0:
+                    sg.PopupNoTitlebar('(Nome de usário/Senha) não cumprem as especificações')
+                else:
+                    #banco.registrar()
+                    window2.close()
+                    window1.un_hide()
 
 test1 = Program()
 test1.loop()

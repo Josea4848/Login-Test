6822e107d89bf61d6ab21710b0895bc939ee1969
José Alves <josealves3416@gmail.com>
2021-09-12 22:44:18 -0300

    Adicionado o banco de dados

--- menu.py
+++ menu.py
@@ -1,10 +1,47 @@
 from tkinter.constants import TRUE
 import PySimpleGUI as sg
 from PySimpleGUI.PySimpleGUI import WINDOW_CLOSED, Button, Prog
+import sqlite3
+from time import sleep
 
 class Program:
     def __init__(self):
         None
+
+    #database    
+    def registrar(self, name, password):
+        self.con = sqlite3.connect('database.db')
+        self.cursor = self.con.cursor()
+
+        try:
+            self.cursor.execute("""CREATE TABLE usuarios (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                senha TEXT NOT NULL
+            )
+
+
+            """)
+        except:
+            print('Erro')
+        sleep(5)
+        
+        self.cursor.execute(f"""INSERT INTO usuarios (nome, senha)
+        VALUES ('{name}', '{password}')""")
+        
+        self.con.commit()
+        self.con.close()
+    #checking values in loguin
+    def verificar(self,name,psw):
+        self.con = sqlite3.connect('database.db')
+        self.cursor = self.con.cursor()
+        self.cursor.execute("""
+        SELECT * FROM usuarios;""")
+
+        for line in self.cursor.fetchall():
+            if name == line[1] and psw == line[2]:
+                return True
+        self.con.close()
     
     #starwindow
     def start_window(self):
@@ -12,8 +49,8 @@ class Program:
         sg.theme('LightBlue')
         layout = [
             [sg.Text('Login Test', font='Bahnschrift 25')],
-            [sg.Text('Usuário'), sg.InputText(size=(60,60))],
-            [sg.Text('Senha  '), sg.InputText(password_char='*',size=(30,30))],
+            [sg.Text('Usuário'), sg.InputText(key='user_login',size=(60,60))],
+            [sg.Text('Senha  '), sg.InputText(key='psw_login',password_char='*',size=(30,30))],
             [sg.Button('Novo usuário?'), sg.Button(button_text='Entrar')]
         ]
 
@@ -40,9 +77,15 @@ class Program:
             window, event, values = sg.read_all_windows()
             if window == window1 and event == sg.WINDOW_CLOSED:
                 break
+            
+            if event == 'Entrar':
+                if self.verificar(values['user_login'],values['psw_login']) == True:
+                    sg.PopupNoTitlebar('ABRIU')
+            
             if event == 'Novo usuário?':
                 window2 = self.rg_window()
                 window1.hide()
+            
             if window == window2 and event == 'Registrar':
                 #temcchar é uma variável do tipo True/False que retorne True se possuir algum caractere indesejado no nome
                 temcchar= False
@@ -52,8 +95,9 @@ class Program:
 
                 if len(values['_username_']) < 8 or len(values['password']) < 8 or temcchar > 0:
                     sg.PopupNoTitlebar('(Nome de usário/Senha) não cumprem as especificações')
+                
                 else:
-                    #banco.registrar()
+                    self.registrar(values['_username_'], values['password'])
                     window2.close()
                     window1.un_hide()
 
